2af53d078fb30a6f8520fdd862a553842ed708fc
eliasyeme <eliasyeme@gmail.com>
2023-11-27 20:02:10 +0300

    counting children

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,2 +1,26 @@
 #include "binary_trees.h"
-size_t binary_tree_nodes(const binary_tree_t *tree);
+
+
+/**
+	* binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
+	* @tree: pointer to the root node of the tree to count the number of nodes.
+	*
+	*	Return: size of the binary tree
+	*					0 otherwise
+	*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		if (tree->left || tree->right)
+			nodes += 1;
+
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+
+	return (nodes);
+}
